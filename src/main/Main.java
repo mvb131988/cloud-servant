d4058ec0d6298d9738615af1f602b163a4b4d226
Vladimir Minacov <Vladimir.Minacov@endava.com>
2017-07-27 11:10:37 +0300

    Converted to maven project

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
+		System.out.println("Run");
 		AppContext context = new AppContext();
 		context.start();
 	}
