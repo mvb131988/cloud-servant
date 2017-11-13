97b3e2f90ce38a0ff48bb03fe59ba34f7274e101
Vladimir Minacov <Vladimir.Minacov@endava.com>
2017-11-13 18:08:42 +0200

    Minor changes

--- launch.bat
+++ launch.bat
@@ -1,4 +1,9 @@
 @REM Run as a javaw process outside command line
-@REM D is unused parameter listed by jps tool
+@REM D is unused parameter listed by jps tool, however it is used by stop.bat script to find jvm that has to be stopped
+@REM 
+@REM To run it on windows startup, but before log on use: 
+@REM Control Panel -> Administrative Tools-> Task Scheduler  
+@REM and create a task that starts before log on. During task creation set path to launch.bat  
 
-javaw -jar -Dcloud-servant cloud-servant.jar start
+start javaw -jar -Dcloud-servant cloud-servant.jar start
+exit
