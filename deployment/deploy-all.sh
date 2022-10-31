d0bbf6d6c289b126fcc3a56c0f9901e9433d3e10
Vladimir Minacov <Vladimir.Minacov@endava.com>
2022-10-31 16:13:55 +0200

    Adjust to ip changes

--- deployment/deploy-all.sh
+++ deployment/deploy-all.sh
@@ -4,7 +4,7 @@ printf "\n### Deploy member2 ###\n"
 ./deploy.sh member2 92.115.183.17
 
 printf "\n### Deploy member3 ###\n"
-./deploy.sh member3 95.65.61.110
+./deploy.sh member3 89.28.113.140
 
 printf "\n### Deploy member4 ###\n" 
 ./deploy.sh member4 188.237.1.27
